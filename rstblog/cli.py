7200f5b970f4519d097313fe2c338daa81065a6f
Daniel Neuhäuser <dasdasich@gmail.com>
2013-01-03 16:39:40 +0100

    Add an --interface option to pick up an IP
    This makes it convenient to serve the blog on the local network, which
    makes developing with mobile devices easier.

--- rstblog/cli.py
+++ rstblog/cli.py
@@ -11,6 +11,7 @@
 from __future__ import with_statement
 import sys
 import os
+import subprocess
 from docopt import docopt
 from rstblog.config import Config
 from rstblog.builder import Builder
@@ -27,18 +28,25 @@ def get_builder(project_folder):
     return Builder(project_folder, config)
 
 
+def get_host(interface="en0"):
+    return subprocess.check_output(
+        ["ipconfig", "getifaddr", interface]
+    ).strip()
+
+
 def main():
     """
     Usage:
         run-rstblog
         run-rstblog build [<folder>]
-        run-rstblog serve [--port <port>] [--host <host>] [<folder>]
+        run-rstblog serve [--port <port>] [(--host <host> | --interface <interface>)] [<folder>]
         run-rstblog -h | --help
 
     Options:
-        -h --help    Show this.
-        --port=port  Port used by server [default: 5000].
-        --host=host  Host used by server [default 127.0.0.1].
+        -h --help              Show this.
+        --port=port            Port used by server [default: 5000].
+        --host=host            Host used by server [default 127.0.0.1].
+        --interface=interface  Use IP of network interface [default: en0].
     """
     arguments = docopt(main.__doc__)
     if arguments['build']:
@@ -52,11 +60,16 @@ def main():
     else:
         folder = arguments['<folder>']
 
+    if arguments["--interface"]:
+        host = get_host(arguments["--interface"])
+    else:
+        host = arguments["--host"]
+
     builder = get_builder(folder)
     if action == 'build':
         builder.run()
     else:
         builder.debug_serve(
             port=int(arguments["--port"]),
-            host=arguments["--host"]
+            host=host
         )
